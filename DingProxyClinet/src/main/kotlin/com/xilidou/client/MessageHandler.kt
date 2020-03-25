2297b6134ab63e9362af367c65f1990cc54392aa
Zhengxin <diaozxin@163.com>
2020-03-25 20:19:54 +0800

    init

--- DingProxyClinet/src/main/kotlin/com/xilidou/client/MessageHandler.kt
+++ DingProxyClinet/src/main/kotlin/com/xilidou/client/MessageHandler.kt
@@ -12,17 +12,14 @@ class MessageHandler (private val eventBus: EventBus): Handler<String> {
 
     val log = LoggerFactory.getLogger(this.javaClass);
 
-    val Hook =
-        "https://oapi.dingtalk.com/robot/send?access_token=1d43c26cf6d2938ffa4454297fbd71389f34ec1b1869ca8818c9c5cc8bc7f58f"
+    val Hook = ""
+
 
     override fun handle(event: String) {
         log.info("event $event")
         val jsonObject = JsonObject(event)
-        val sessionWebhook = jsonObject.getString("sessionWebhook")
         val TextContent = jsonObject.getJsonObject("text").getString("content")
 
-
-//        Wechaty bot = Wechaty.instance();
         val client = DefaultDingTalkClient(Hook)
 
         val request = OapiRobotSendRequest()
